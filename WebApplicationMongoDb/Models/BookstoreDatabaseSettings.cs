3fca595f27ee242a22e8852f03d4ec8ec4a3ffbf
hoibuikms <hoibui@kms-technology.com>
2020-05-21 16:32:59 +0700

    first commit

---
+++ WebApplicationMongoDb/Models/BookstoreDatabaseSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplicationMongoDb.Constract;
+
+namespace WebApplicationMongoDb.Models
+{
+	public class BookstoreDatabaseSettings : IBookstoreDatabaseSettings
+	{
+		public string BooksCollectionName { get; set; }
+		public string ConnectionString { get; set; }
+		public string DatabaseName { get; set; }
+	}
+}
